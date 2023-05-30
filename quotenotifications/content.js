e6f2f2f51d93473edd05744918aede44f72bd49b
Al <alexander.chansky@gmail.com>
2023-05-30 17:57:57 -0400

    yep

--- quotenotifications/content.js
+++ quotenotifications/content.js
@@ -1,19 +1,25 @@
-// async function startRequest() {
-    
-//     const response = await fetch('https://api.quotable.io/random'); // pulls json from an API
-//     const newData = await response.json(); // 
-//     const data = `${newData.content} —${newData.author}` // what will be printed to notification
-//     console.log(data); // how to actually print to notification
-//     // chrome.storage.sync.set({data});
-    
-//     var options = {
-//       title: 'Random Quotes',
-//       message: data,
-//       iconUrl: "readingImages/icon-16.png",
-//       type: 'basic',
-//       // requireInteraction: true
-//     }
-//     chrome.notifications.create('', options);
-// }
+//alert('Hello, world!'); // cannot be done on google URLs for some reason
 
-alert('Hello, world!');
+// Add an event listener to trigger startRequest function when the button is clicked
+document.getElementById('myButton').addEventListener('click', () => {
+    startRequest();
+  });
+
+async function startRequest() {
+    
+    const response = await fetch('https://api.quotable.io/random');
+    const newData = await response.json();
+    const data = `${newData.content} —${newData.author}`
+    console.log(data);
+    // chrome.storage.sync.set({data});
+    
+    
+    var options = {
+        title: 'Random Quotes',
+        message: data,
+        iconUrl: 'readingImages/icon-16.png',
+        type: 'basic',
+        // requireInteraction: true
+    }
+    chrome.notifications.create('', options);
+}
