5b3f1055ede96a4cb120175a083bff310fb2b505
Al <alexander.chansky@gmail.com>
2023-05-31 16:59:06 -0400

    change these

--- quotenotifications/background_v2.js
+++ quotenotifications/background_v2.js
@@ -1,6 +1,8 @@
+// this successfully runs without any errors -- but does not complete action expected
+
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
       target: {tabId: tab.id},
-      files: ['content.js']
+      files: ['content_v2.js']
     });
   });
